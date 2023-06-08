77680589ed14a4039c17359a6888328d41f12b6a
Mohammad <montazeriyazd1@gmail.com>
2023-06-09 02:17:54 +0330

    link the back end and front end

--- back end/website/views.py
+++ back end/website/views.py
@@ -92,35 +92,6 @@ def upload():
     data = {'products': products}
     return render_template('add-product.html', data=data)
 
-
-@views.route('/add-to-cart', methods=['POST'])
-def addCart():
-    product_name = request.form.get('productName')
-    product = Products.objects.get(name=product_name)
-    new_cart_product = Cart(product_name=Products.product_name, price_toman=Products.price_toman, price_dollar=Products.price_dollar, count=Products.count, gdrive_link=Products.gdrive_link)
-    db.session.add(new_cart_product)
-    db.session.commit()
-    return redirect(url_for('views.index'))
-
-
-@views.route('decrese-product', methods=['POST'])
-def decrease_product():
-    product_name = json.loads(request.data('product'))
-    product = Products.objects.get(name=product_name)
-    
-    db.session.delete(product)
-    if product and product.count > 1:
-        product.count -= 1
-
-
-@views.route('increase-product', methods=['POST'])
-def increase_product():
-    product_name = json.loads(request.data('product'))
-    product = Products.objects.get(name=product_name)
-    if product:
-        product.count += 1
-
-
 @views.route("/remove-product", methods=["POST"])
 def remove_product():
   data = json.loads(request.data)
@@ -128,6 +99,69 @@ def remove_product():
   product_item = Products.query.filter_by(product_name=product_name).first()
   if product_item:
     db.session.delete(product_item)
+    db.session.commit()
+    return jsonify({"success": True})
+  else:
+    return jsonify({"success": False})
+  
+@views.route("/add-to-cart", methods=["POST"])
+def add_to_cart():
+  data = json.loads(request.data)
+  product_name = data["productName"]
+  product = Products.query.filter_by(product_name=product_name).first()
+  product_cart = Cart.query.filter_by(product_name=product_name).first()
+  if product:
+      if product_cart:
+         product_cart.count +=1
+         db.session.commit()
+         return jsonify({"success": True})
+      else:
+        price_toman = product.price_toman
+        gdrive_link = product.gdrive_link
+        price_dollar = round(price_toman / dollar ,2)
+        count = 1
+        total_toman = count * price_toman
+        total_dollar = count * price_dollar
+        new_cart_product = Cart(product_name=product_name,price_toman=price_toman,price_dollar=price_dollar,count=count,total_toman=total_toman,total_dollar=total_dollar,gdrive_link=gdrive_link,)
+        db.session.add(new_cart_product)
+        db.session.commit()
+        return jsonify({"success": True})
+  else:
+    return jsonify({"success": False})
+  
+@views.route("/remove-from-cart", methods=["POST"])
+def remove_from_cart():
+  data = json.loads(request.data)
+  product_name = data["productName"]
+  product_item = Cart.query.filter_by(product_name=product_name).first()
+  if product_item:
+    db.session.delete(product_item)
+    db.session.commit()
+    return jsonify({"success": True})
+  else:
+    return jsonify({"success": False})
+  
+@views.route("/in-count", methods=["POST"])
+def in_count():
+  data = json.loads(request.data)
+  product_name = data["productName"]
+  product_item = Cart.query.filter_by(product_name=product_name).first()
+  if product_item:
+    print(product_item.count)
+    product_item.count += 1
+    product_item.count
+    db.session.commit()
+    return jsonify({"success": True})
+  else:
+    return jsonify({"success": False})
+  
+@views.route("/de-count", methods=["POST"])
+def de_count():
+  data = json.loads(request.data)
+  product_name = data["productName"]
+  product_item = Cart.query.filter_by(product_name=product_name).first()
+  if product_item and product_item.count > 1:
+    product_item.count -= 1
     db.session.commit()
     return jsonify({"success": True})
   else:
